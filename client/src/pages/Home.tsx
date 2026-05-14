a9fffd369d998a19799a2ffddd217b4092e9aee8
Ausar <ausar@Ausar-lap-5.local>
2026-05-14 17:28:51 -0400

    Reduce height of Hero-to-Bio gradient transition

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -229,7 +229,7 @@ export default function Home() {
         </motion.div>
 
         {/* Gradient Transition to Bio Section */}
-        <div className="absolute bottom-0 left-0 w-full h-48 md:h-64 bg-gradient-to-t from-[#F5F0EB] via-[#F5F0EB]/60 to-transparent z-10 pointer-events-none" />
+        <div className="absolute bottom-0 left-0 w-full h-10 md:h-16 bg-gradient-to-t from-[#F5F0EB] via-[#F5F0EB]/60 to-transparent z-10 pointer-events-none" />
       </section>
 
       {/* ============ BIO SECTION (Light) ============ */}
